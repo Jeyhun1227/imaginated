20f05cc147c83c08f2ab4924f6caf38cde2f718f
Po245810@G <pouya.bavafa@gmail.com>
2022-10-02 17:08:25 -0700

    trying to add search dropdown

--- components/NavBars/headers.js
+++ components/NavBars/headers.js
@@ -62,23 +62,41 @@ export default function Header({placeholder = 'Search for a creator or category'
           <a href="/" className="flex items-center mr-3">
               <img src="/imaginated_logo.png" className="xl:h-10 sm:h-5 md:h-7" alt="Imaginated Logo" />
           </a>
-          <Combobox value={searchTerm} onChange={setSearchTerm}>
+          <Combobox as="li" value={searchTerm} onChange={setSearchTerm} className="relative list-none">
             <div className="items-start justify-start hidden xl:w-4/12 lg:w-3/12 sm:flex sm:order-1">
               <label htmlFor="simple-search" className="sr-only">Search</label>
               <div className="relative w-full">
                   <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                       <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                   </div>
-                  <Combobox.Input type="text" id="simple-search" data-dropdown-toggle="dropdown" className="block py-2 text-sm text-gray-900 border border-gray-300 text-ellipsis xl:pr-20 sm:pr-10 w-96 pl-11 sm:w-full focus:outline-none" placeholder={placeholder} required onChange={(e) => setSearchTerm(e.target.value)}/>
+                  <Combobox.Input type="text" id="simple-search" data-dropdown-toggle="dropdown" className="block py-2 text-sm border text-ellipsis xl:pr-20 sm:pr-10 w-96 pl-11 sm:w-96 focus:outline-none" placeholder={placeholder} required onChange={(e) => setSearchTerm(e.target.value)}/>
               </div>
             </div>
-            <Combobox.Options>
+            <Combobox.Options className="absolute z-10 py-1 mt-1 overflow-x-hidden overflow-y-auto text-base bg-white shadow-lg w-96 top-10 max-h-56 focus:outline-none sm:text-sm">
               {
-                searchResult.map((result) => (
-                <div>
-                  <Combobox.Option>{result.fullname}</Combobox.Option>
-                  <Combobox.Option>{result.subcategory}</Combobox.Option>
-                  <Combobox.Option>{result.imagelink}</Combobox.Option>
+                searchResult.filter(result => result.subcategory == subcategory).map((result, i) => (
+                <div className='my-2'>
+                  {result.subcategory ?
+                  <Combobox.Option>
+                    <div className="flex flex-row">
+                      <div className="pr-4">
+                        <img className="w-8 h-8 rounded-full" src={result.imagelink ? result.imagelink : "/user.png"}/>
+                      </div>
+                      <div>
+                        <div className="mb-1 font-semibold">{result.fullname}</div>
+                        <div className="flex flex-row space-x-4">
+                          <div className="inline-flex items-center justify-center space-x-2">{result.subcategory.map((e) => <div className="truncate text-xSsm text-dim-grey">{e}</div>)}</div>
+                        </div>
+                      </div>
+                    </div>
+                  </Combobox.Option>
+                  : <Combobox.Option>
+                  <div className="flex flex-row">
+                    <div>
+                      <div className="font-semibold">{result.fullname}</div>
+                    </div>
+                  </div>
+                </Combobox.Option>}
                 </div>
               ))}
             </Combobox.Options>
