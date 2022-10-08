da2d31dfeaa9cf17ebf3b7bf8ea5c6b1835409fe
Soroosh Nikaein <soroosh@Sorooshs-MacBook-Pro.local>
2022-10-08 13:51:37 -0700

    added fixes to imports

--- next.config.js
+++ next.config.js
@@ -3,16 +3,22 @@ const webpack = require('webpack');
 module.exports = {
   reactStrictMode: true,
   plugins: [
-    new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/ }),
-    new webpack.IgnorePlugin(/^pg-native$/ )
-
+    new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/ })
   ],
-  async rewrites() {
-    return [
-      {
-        source: '/graphql',
-        destination: 'https://api.imaginated.com/graphql/:path*' // Proxy to Backend
-      }
-    ]
-  },
+  webpack: (config) => {
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^pg-native$/,
+      })
+    );
+    return config;
+  }
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/graphql',
+  //       destination: 'https://api.imaginated.com/graphql/:path*' // Proxy to Backend
+  //     }
+  //   ]
+  // },
 }
